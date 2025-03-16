ae4e6a381e337fbdcdfb483195f795b7de34c46e
xpadro <xpadro.sw@gmail.com>
2025-03-16 10:16:51 +0100

    add link to job title

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -8,7 +8,9 @@
     <div class="flex-1 flex flex-col">
         <a href="" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{ $job->title }}</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">
+            <a href="{{ $job->url }}" target="_blank">{{ $job->title }}</a>
+        </h3>
         <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
